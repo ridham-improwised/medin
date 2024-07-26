11e14eb0fa8474f25e8a4189b7b52977496bf5b5
Ridham Parmar <143784359+ridham-improwised@users.noreply.github.com>
2024-07-26 13:40:59 +0530

    Update index.tsx
    Signed-off-by: Ridham Parmar <143784359+ridham-improwised@users.noreply.github.com>

--- medin-storefront/src/modules/store/templates/index.tsx
+++ medin-storefront/src/modules/store/templates/index.tsx
@@ -1,8 +1,8 @@
 import { Suspense } from "react"
-
 import SkeletonProductGrid from "@modules/skeletons/templates/skeleton-product-grid"
 import RefinementList from "@modules/store/components/refinement-list"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
+import Image from "next/image"
 
 import PaginatedProducts from "./paginated-products"
 
@@ -16,7 +16,7 @@ const StoreTemplate = ({
   countryCode: string
 }) => {
   const pageNumber = page ? parseInt(page) : 1
-
+  const baseUrl = "https://imageproxy.artisanscloud.com.my" + "/sig/quality:100/plain/https://medusa-public-images.s3.eu-west-1.amazonaws.com/coffee-mug.png@webp" ;
   return (
     <div className="flex flex-col small:flex-row small:items-start py-6 content-container" data-testid="category-container">
       <RefinementList sortBy={sortBy || "created_at"} />
@@ -24,6 +24,14 @@ const StoreTemplate = ({
         <div className="mb-8 text-2xl-semi">
           <h1 data-testid="store-page-title">All products</h1>
         </div>
+        <Image
+          src={baseUrl}
+          alt="proxy image"
+          draggable={false}
+          quality={100}
+          sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
+          fill
+        />
         <Suspense fallback={<SkeletonProductGrid />}>
           <PaginatedProducts
             sortBy={sortBy || "created_at"}
